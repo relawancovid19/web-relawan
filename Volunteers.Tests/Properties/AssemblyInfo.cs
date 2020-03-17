472e31148b6abc2930501969b2381a115f1c9a4a
cloudcomputingID <alex@cloudcomputing.id>
2020-03-17 16:57:31 +0700

    Fixing initial project :)

--- Volunteers.Tests/Properties/AssemblyInfo.cs
+++ Volunteers.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Volunteers")]
+[assembly: AssemblyTitle("Volunteers.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Volunteers")]
+[assembly: AssemblyProduct("Volunteers.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("871f7330-a824-4369-adcc-ac67fea495a4")]
+[assembly: Guid("fe957bd4-30cd-4e96-81b4-b2c7792038d3")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ Volunteers/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Volunteers.Startup))]
+namespace Volunteers
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
