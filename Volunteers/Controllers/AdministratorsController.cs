e1a66ad1344e3c3c4a791a4325fc4ad314f95bb5
bukharkan <bukharkan@gmail.com>
2020-03-17 23:32:36 +0700

    added administrators page updated table jobs

---
+++ Volunteers/Controllers/AdministratorsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Volunteers.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    public class AdministratorsController : Controller
+    {
+        // GET: Administrators
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public async Task<ActionResult> Jobs()
+        {
+            return View();
+        }
+    }
+}
